0cf4273da426b87d0b716961d5f8761ec4a60f97
diogoecommerce798-blip <diogoecommerce798@gmail.com>
2026-04-08 15:40:55 -0300

    Add files via upload

--- server/db.ts
+++ server/db.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
-import * as schema from "../drizzle/schema";
+import * as schema from "../drizzle/schema.js";
 import { ENV } from "./_core/env";
 import { eq } from "drizzle-orm";
 
